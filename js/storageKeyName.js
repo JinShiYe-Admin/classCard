cae62c321ed0b4bb4de8ba4ac39707c6dbee3a5c
Zqw <578159259@qq.com>
2018-09-12 15:33:31 +0800

    修改

--- js/storageKeyName.js
+++ js/storageKeyName.js
@@ -29,8 +29,8 @@ var storageKeyName = (function(mod) {
 	}
 	mod.PERSONALINFO = 'personalInfo1111'; //个人信息，登录成功后返回值
 	mod.SHAKEHAND = 'ShakeHand'; //公钥，登录时，返回的握手信息，
-
 	mod.PUBLICPARAMETER = 'publicParameter' //共用参数
+	
 
 	mod.WAITING = '加载中...'; //
 	mod.UPLOADING = '上传中...';
